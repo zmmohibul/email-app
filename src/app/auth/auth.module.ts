cb556c6f4b9b9db7c5a5255abe8ea457b695e31e
zmmohibul <18-36427-1@student.aiub.edu>
2022-01-27 21:02:04 +0600

    Auth Module Signup Form - Async Username Validator

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
+import {HttpClientModule} from "@angular/common/http";
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { SignupComponent } from './signup/signup.component';
   imports: [
     CommonModule,
     AuthRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
 })
 export class AuthModule { }
---
+++ src/app/auth/validators/unique-username.ts
@@ -0,0 +1,18 @@
+import {Injectable} from "@angular/core";
+import {AsyncValidator, FormControl} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: "root"
+})
+export class UniqueUsername implements AsyncValidator{
+  constructor(private http: HttpClient) { }
+
+  validate = (control: FormControl) => {
+    const { value } = control;
+
+    return this.http.post<any>('https://api.angular-email.com/auth/username', {
+      username: value
+    });
+  }
+}
