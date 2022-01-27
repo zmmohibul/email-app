199d3ab98878ccd85e5cb4eba8558a785ec8873b
zmmohibul <18-36427-1@student.aiub.edu>
2022-01-27 21:11:14 +0600

    Auth Module Signup Form - Async Username Validator Handle Err

--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -1,6 +1,8 @@
 import {Injectable} from "@angular/core";
 import {AsyncValidator, FormControl} from "@angular/forms";
 import {HttpClient} from "@angular/common/http";
+import {catchError, map} from "rxjs/operators";
+import {of} from "rxjs";
 
 @Injectable({
   providedIn: "root"
@@ -13,6 +15,19 @@ export class UniqueUsername implements AsyncValidator{
 
     return this.http.post<any>('https://api.angular-email.com/auth/username', {
       username: value
-    });
+    }).pipe(map(value => {
+        if (value.available) {
+          return null;
+        }
+      }),
+      catchError(err => {
+        if (err.error.username) {
+          return of({ nonUniqueUsername: true });
+        } else {
+          return of({ noConnection: true });
+        }
+      })
+    );
+
   }
 }
