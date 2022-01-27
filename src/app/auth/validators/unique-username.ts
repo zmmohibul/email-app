49f31e45a44f89d525a4332f63a478ca75c9edae
zmmohibul <18-36427-1@student.aiub.edu>
2022-01-27 21:15:32 +0600

    Auth Module Signup Form - Auth Service

--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -3,19 +3,18 @@ import {AsyncValidator, FormControl} from "@angular/forms";
 import {HttpClient} from "@angular/common/http";
 import {catchError, map} from "rxjs/operators";
 import {of} from "rxjs";
+import {AuthService} from "../auth.service";
 
 @Injectable({
   providedIn: "root"
 })
 export class UniqueUsername implements AsyncValidator{
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authService:AuthService) { }
 
   validate = (control: FormControl) => {
     const { value } = control;
 
-    return this.http.post<any>('https://api.angular-email.com/auth/username', {
-      username: value
-    }).pipe(map(value => {
+    return this.authService.usernameAvailable(value).pipe(map(value => {
         if (value.available) {
           return null;
         }
